68ee67f1bbc26ff558341b5089978a546311ec95
Fazliradika <49340780+Fazliradika@users.noreply.github.com>
2026-05-11 11:03:03 +0700

    Add xsh_memlab to display memory segment information
    Implement memory lab shell command to display segment addresses.

---
+++ xsh_memlab.c
@@ -0,0 +1,33 @@
+#include <xinu.h>
+#include <stdio.h>
+
+int global_data_var = 42;
+int global_bss_var;
+void text_marker() {}
+
+shellcmd xsh_memlab(int argc, char *argv[]) {
+        int stack_var;
+        char *heap_ptr;
+        uint32 size = 1024;
+
+        printf("\n === Praktikum By EL Goat ===");
+
+        //Tampilan Alamat Segmen
+        printf("\n1. Pemetaan Segmen Statis:\n");
+        printf("\n [Text] Alamat Fungsi : %08x", (uint32)&text_marker);
+        printf("\n [Data] Variabel Glob : %08x", (uint32)&global_data_var);
+        printf("\n [BSS] Variabel Glob  : %08x", (uint32)&global_bss_var);
+
+        //Tampilan Stack vs Heap
+        heap_ptr = (char *)getmem(size);
+        printf("\n\n2. Alokasi Dinamis");
+        printf(" \n[Stack] Alamat Lokal : %08x", (uint32)&stack_var);
+
+        if ((int32)heap_ptr != SYSERR) {
+                printf(" \n[Heap] Alamat getmem : %08x", (uint32)heap_ptr);
+                freemem(heap_ptr, size);
+        }
+
+        printf("\n================================\n\n");
+        return 0;
+}
